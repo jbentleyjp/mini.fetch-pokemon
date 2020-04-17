d8c8e5dfc31a6ff74e488077fecb322ea549cf88
maegatro <shuntaromaekawa@gmail.com>
2020-04-17 16:56:04 +0900

    First commit, completion of find names.
    Co-authored-by: maegatro <maegatro@users.noreply.github.com>
    Co-authored-by: jbentleyjp <jbentleyjp@users.noreply.github.com>

--- pokemonager.js
+++ pokemonager.js
@@ -1,10 +1,11 @@
 (() => {
   class Pokemonager {
     // This should return an array of all the names of n Pokemon from the Pokemon API.
-    findNames(n) {
-      // Your code here.
+    async findNames(n) {
+      let resolvePoke = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${n}`);
+      const pokemons = await resolvePoke.json();
+      return pokemons.results.map(pokemon => pokemon.name);
     }
-
     // This should return an array of all the Pokemon that are under a particular weight.
 
     findUnderWeight(weight) {
